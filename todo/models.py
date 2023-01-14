d0933fc10864cbdf7f575a7ad7f68a9141141981
Андрей <overDev@mail.ru>
2023-01-14 14:54:11 +0300

    Stage 4. Ready

---
+++ todo/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class Todo(models.Model):
+    title = models.CharField(max_length=150)
+    description = models.CharField(max_length=300, blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+    done = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.title} - {self.done}"
